98f665d95cccffcb72b71c1d648727a7b949da9d
chethansbsc24-design <chethansbsc24@rvu.edu.in>
2026-03-26 12:42:50 +0530

    first commit

--- cie2.py
+++ cie2.py
@@ -1,1 +1,17 @@
-print("helloooo")
+n = int(input("Enter an integer: "))
+
+# Step 1: Find sum of digits
+digit_sum = 0
+temp = n
+
+while temp > 0:
+    digit_sum += temp % 10
+    temp //= 10
+
+# Step 2: Check if sum is palindrome
+sum_str = str(digit_sum)
+
+if sum_str == sum_str[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
